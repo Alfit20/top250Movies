ff0676c87bac9f464c7d13e31bf07385887ae3e9
Alfit20 <alfit.bashirov@gmail.com>
2022-12-17 01:03:01 +0600

    Added store

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,5 +1,13 @@
 import axios from "axios";
+import interceptors from "@/plugins/axios/interceptors";
 
-const intance = axios.create({});
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  // params: {
+  //   apikey: process.env.VUE_APP_API_KEY,
+  //   plot: "full",
+  // },
+});
+interceptors(instance);
 
-export default intance;
+export default instance;
